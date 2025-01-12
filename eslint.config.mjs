a2b0f7dab3216ee6ac41f86bfeb6c46bdfebcec9
Amin-Zoghlami <amin.zoghlami.361@my.csun.edu>
2025-01-11 21:23:10 -0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,5 @@ export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  {
-    excludedFiles: "**/webpack.*.js",
-  },
+  { ignores: ["**/webpack.*.js"] },
 ];
